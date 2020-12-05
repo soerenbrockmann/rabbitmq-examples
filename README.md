2d0bf8a6032c9f04922d1481dee3ca89ed14c58f
S <s@sb.local>
2020-12-05 20:08:42 +0100

    Add simple examples for go python and js

---
+++ README.md
@@ -0,0 +1,11 @@
+# RabbitMQ examples
+
+## Start RabbitMQ
+
+To have launchd start rabbitmq:
+
+`brew services start rabbitmq`
+
+Start service in terminal:
+
+`rabbitmq-server`
---
+++ hello-js/src/receive.js
@@ -0,0 +1,35 @@
+import amqp from "amqplib/callback_api";
+
+amqp.connect("amqp://localhost", (error0, connection) => {
+  if (error0) {
+    throw error0;
+  }
+
+  connection.createChannel((error1, channel) => {
+    if (error1) {
+      throw error1;
+    }
+
+    const queue = "hello";
+
+    channel.assertQueue(queue, {
+      durable: false,
+    });
+
+    console.log("[*] Waiting for messages in %s. To exit press CTRL+C", queue);
+    channel.consume(
+      queue,
+      (msg) => {
+        console.log("[*] Received %s", msg.content.toString());
+      },
+      {
+        noAck: true,
+      }
+    );
+  });
+
+  setTimeout(() => {
+    connection.close();
+    process.exit(0);
+  }, 500);
+});
